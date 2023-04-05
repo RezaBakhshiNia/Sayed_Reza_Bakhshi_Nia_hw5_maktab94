f717d704b3e08ed484ed3e75dd83f040c482f2fd
Reza Bakhshi Nia <rezahavestax@gmail.com>
2023-04-05 23:34:45 +0330

    Second JS question was solved.
    Making a pedometer on object by making methods.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,3 +10,30 @@ const user = {
 };
 
 user.sayHello();
+
+// 2
+const pedometer = {
+  steps: 0,
+  increase() {
+    this.steps++;
+  },
+  decrease() {
+    this.steps--;
+  },
+  reset() {
+    this.steps = 0;
+  },
+  read() {
+    console.log(`Number of steps: ${this.steps}`);
+  },
+};
+
+pedometer.increase();
+pedometer.increase();
+pedometer.read(); // Output: "Number of steps: 2"
+
+pedometer.decrease();
+pedometer.read(); // Output: "Number of steps: 1"
+
+pedometer.reset();
+pedometer.read(); // Output: "Number of steps: 0"
